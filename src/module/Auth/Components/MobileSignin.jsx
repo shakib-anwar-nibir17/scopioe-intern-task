72b43323eec974f3ff8755420c936f45b41cfa2c
shakib-anwar-nibir17 <nibirshakib@gmail.com>
2024-08-04 20:42:13 +0600

    siging page mobile view

--- src/module/Auth/Components/MobileSignin.jsx
+++ src/module/Auth/Components/MobileSignin.jsx
@@ -1,16 +1,14 @@
 import { Button } from '../../../Components/buttons/button';
 import MainLogo from '../../../Components/Logos/MainLogo';
 import { SimplePagination } from '../../../Components/pagination/simplePagination';
-import Facebook from '../../../Components/SocialSignup/Facebook';
-import Google from '../../../Components/SocialSignup/Google';
 import { Text } from '../../../Components/text/Text';
-import LoginForm from './LoginForm';
 import MobileAuth from './MobileAuth';
 
 import { useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import SigninForm from './SigninForm';
 
-const MobileLogin = () => {
+const MobileSignin = () => {
   const location = useLocation();
   const [open, setOpen] = useState(false);
   return (
@@ -21,16 +19,26 @@ const MobileLogin = () => {
           <Text variant="headerMedium" type="bold" className={`mb-6 mt-8 opacity-70`}>
             Login to Your Account
           </Text>
+          <Text className={`text-white opacity-70`} variant="bodyRegular">
+            Welcome Back! By click the sign up button, you&apos;re agree to Zenitood Terms and
+            Service and acknowledge the{' '}
+            <Button variant="link" size="null">
+              Privacy and Policy
+            </Button>
+          </Text>
         </div>
 
         <Button
           onClick={() => setOpen(!open)}
           variant="ghost"
           size="null"
-          className="mx-auto flex h-[14.3rem] w-[calc(100%-3rem)] items-center justify-center rounded-lg bg-[#1F2833] text-center opacity-70"
+          className="mx-auto flex h-[14.3rem] w-[calc(100%-3rem)] flex-col items-center justify-center rounded-lg bg-[#1F2833] text-center opacity-70"
         >
+          <Text variant="bodyLarge" type="medium" className={`text-primary`}>
+            Create Account
+          </Text>
           <Text variant="bodyLarge" type="medium" className={`text-white`}>
-            <span className="text-primary"> Sign In</span> to view all the <br /> massage therapists
+            Fill your Information
           </Text>
         </Button>
         <div className="flex justify-center">
@@ -40,35 +48,18 @@ const MobileLogin = () => {
       <section className={`${open ? 'block' : 'hidden'} h-full w-full`}>
         <div className="flex h-[27%] w-full flex-col items-center justify-center bg-transparent pt-11 text-md text-white">
           <MainLogo />
-          <Text className={`mt-6 w-1/2 text-center text-md opacity-70`}>
-            Sign In to view all the massage therapists
-          </Text>
+          <Text className={`mt-6 text-md opacity-70`}>Create Account</Text>
+          <Text className="text-md opacity-70">Fill your information</Text>
         </div>
         <div className="h-[73%] w-full rounded-t-[4rem] bg-white px-4 py-6">
           <Text variant="headerMedium" type="medium" className={`mb-6 mt-4 text-center`}>
-            Log In To Your Account
+            Sign in
           </Text>
-          <Text className="text-center" variant="bodyRegular">
-            Welcome Back! Select a method to log in
-          </Text>
-          <div className="mt-4 flex gap-5">
-            <Google />
-            <Facebook />
-          </div>
-          <div className="relative my-8">
-            <hr />
-            <Text
-              variant="sub"
-              className="absolute inset-x-1/2 w-[70%] -translate-x-1/2 -translate-y-1/2 transform bg-white px-10 text-center text-[#5C635A]"
-            >
-              Or Continue with Email
-            </Text>
-          </div>
-          <LoginForm />
+          <SigninForm />
         </div>
       </section>
     </MobileAuth>
   );
 };
 
-export default MobileLogin;
+export default MobileSignin;
--- src/module/Auth/Pages/SigninPage.jsx
+++ src/module/Auth/Pages/SigninPage.jsx
@@ -1,7 +1,25 @@
-import DesktopSignin from '../Components/DesktopSignin';
+import { lazy, Suspense } from 'react';
+import useDeviceType from '../../../hooks/useDeviceTypes';
+
+const DesktopSignin = lazy(() => {
+  return import('../Components/DesktopSignin');
+});
+const MobileSignin = lazy(() => {
+  return import('../Components/MobileSignin');
+});
 
 const SigninPage = () => {
-  return <DesktopSignin />;
+  const deviceType = useDeviceType();
+  console.log(deviceType);
+  const renderLoginComponent = () => {
+    if (deviceType === 'desktop') {
+      return <DesktopSignin />;
+    } else {
+      return <MobileSignin />;
+    }
+  };
+
+  return <Suspense fallback={<div>Loading...</div>}>{renderLoginComponent()}</Suspense>;
 };
 
 export default SigninPage;
